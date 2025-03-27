7846d3aead4c8062d0894f4774cc54d02b603e73
Finochiom <matias.finochio@davinci.edu.ar>
2025-03-27 16:55:52 -0300

    added a collision to 1 tile to test.

--- res_workbench/tiled/Overworld8x8.tsx
+++ res_workbench/tiled/Overworld8x8.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Overworld" tilewidth="8" tileheight="8" tilecount="2232" columns="24" tilerendersize="grid">
  <image source="../../../../Downloads/Mini-Medieval-8x8/Overworld.png" width="192" height="744"/>
+ <tile id="217">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
 </tileset>
